13f80b81d755b853565094a4115679ba405aabf5
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2025-01-25 14:06:57 +0500

    feat: added audio check button

--- Air/Air/ViewModels/AudioDeviceItemViewModel.cs
+++ Air/Air/ViewModels/AudioDeviceItemViewModel.cs
@@ -1,15 +1,32 @@
+using System;
 using System.Windows.Input;
 
 namespace Air.ViewModels;
 
 public class AudioDeviceItemViewModel : ViewModelBase
 {
-    public AudioDeviceItemViewModel(string header, ICommand command)
+    public static AudioDeviceItemViewModel OutputDevice(string header, Guid outputDeviceGuid)
+        => new AudioDeviceItemViewModel(header, outputDeviceGuid);
+
+    public static AudioDeviceItemViewModel InputDevice(string header, string inputDeviceId)
+        => new AudioDeviceItemViewModel(header, inputDeviceId);
+    
+    private AudioDeviceItemViewModel(string header, Guid outputDeviceGuid)
     {
         Header = header;
-        Command = command;
+        OutputDeviceGuid = outputDeviceGuid;
+        InputDeviceId = null;
+    }
+
+    private AudioDeviceItemViewModel(string header, string? inputDeviceId)
+    {
+        Header = header;
+        OutputDeviceGuid = Guid.Empty;
+        InputDeviceId = inputDeviceId;
     }
     
     public string Header { get; set; }
-    public ICommand Command { get; set; }
+    public ICommand? Command { get; set; }
+    public Guid OutputDeviceGuid { get; set; }
+    public string? InputDeviceId { get; set; }
 }
