f127bdd6d19ff582a5d8774723f8978073baf128
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2025-02-02 22:39:30 +0500

    feat: added connection error messages for CallPanelView

--- Air/Air/ViewModels/CallPanelViewModel.cs
+++ Air/Air/ViewModels/CallPanelViewModel.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Air.Connection;
+using Avalonia.Media;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Exception = System.Exception;
 
 namespace Air.ViewModels;
 
@@ -18,17 +20,35 @@ public partial class CallPanelViewModel : BasePanelViewModel
         SubscribeToCallEvents();
     }
     
+    public static TextTrimming ErrorMessageTrimming => TextTrimming.CharacterEllipsis;
+    
     public ObservableCollection<ChatMessageViewModel> ChatMessages { get; } = new();
 
     [ObservableProperty] private string _userMessage = string.Empty;
+    [ObservableProperty] private string? _errorMessage;
+    [ObservableProperty] private string? _errorMessageToolTip;
 
     private readonly CallConnectionManager _callConnectionManager;
+    
+    partial void OnErrorMessageChanged(string? value)
+    {
+        ErrorMessageToolTip = value;
+    }
 
     [RelayCommand(CanExecute = nameof(IsValidMessage))]
     private async Task SendMessage(string message)
     {
-        await _callConnectionManager.SendTextMessage(message);
-        Dispatcher.UIThread.Post(() => UserMessage = string.Empty);
+        try
+        {
+            await _callConnectionManager.SendTextMessage(message);
+            Dispatcher.UIThread.Post(() => UserMessage = string.Empty);
+            Dispatcher.UIThread.Post(() => ErrorMessage = null);
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = e.Message;
+            // TODO: try to reconnect
+        }
     }
 
     [RelayCommand]
